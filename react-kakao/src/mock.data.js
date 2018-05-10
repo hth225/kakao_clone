e2a5120071e74ad7cdeb9380380046fe2f0eeddc
river-stone-jin <sukjinjjang@woowahan.com>
2018-05-10 16:21:31 +0900

    init react kakao clone project

---
+++ react-kakao/src/mock.data.js
@@ -0,0 +1,11 @@
+export const chatRoomInfoList = [
+  { roomName: '주현', messageCount: '2', previewMessage: '보고싶다', recentHistory: 'Today', roomImage: 'http://www.mbcsportsplus.com/images/img.php?srv=1&type=a&src=/2018/02/20180202144833_tlsqdkde.jpg' },
+  { roomName: '주현', messageCount: '2', previewMessage: '보고싶다', recentHistory: 'Today', roomImage: 'http://www.mbcsportsplus.com/images/img.php?srv=1&type=a&src=/2018/02/20180202144833_tlsqdkde.jpg' },
+  { roomName: '주현', messageCount: '2', previewMessage: '보고싶다', recentHistory: 'Today', roomImage: 'http://www.mbcsportsplus.com/images/img.php?srv=1&type=a&src=/2018/02/20180202144833_tlsqdkde.jpg' },
+  { roomName: '주현', messageCount: '2', previewMessage: '보고싶다', recentHistory: 'Today', roomImage: 'http://www.mbcsportsplus.com/images/img.php?srv=1&type=a&src=/2018/02/20180202144833_tlsqdkde.jpg' },
+  { roomName: '주현', messageCount: '2', previewMessage: '보고싶다', recentHistory: 'Today', roomImage: 'http://www.mbcsportsplus.com/images/img.php?srv=1&type=a&src=/2018/02/20180202144833_tlsqdkde.jpg' },
+  { roomName: '주현', messageCount: '2', previewMessage: '보고싶다', recentHistory: 'Today', roomImage: 'http://www.mbcsportsplus.com/images/img.php?srv=1&type=a&src=/2018/02/20180202144833_tlsqdkde.jpg' },
+  { roomName: '주현', messageCount: '2', previewMessage: '보고싶다', recentHistory: 'Today', roomImage: 'http://www.mbcsportsplus.com/images/img.php?srv=1&type=a&src=/2018/02/20180202144833_tlsqdkde.jpg' },
+  { roomName: '주현', messageCount: '2', previewMessage: '보고싶다', recentHistory: 'Today', roomImage: 'http://www.mbcsportsplus.com/images/img.php?srv=1&type=a&src=/2018/02/20180202144833_tlsqdkde.jpg' },
+  { roomName: '주현', messageCount: '2', previewMessage: '보고싶다', recentHistory: 'Today', roomImage: 'http://www.mbcsportsplus.com/images/img.php?srv=1&type=a&src=/2018/02/20180202144833_tlsqdkde.jpg' },
+];
